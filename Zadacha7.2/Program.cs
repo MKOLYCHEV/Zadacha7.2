4681b62986310e1d0ed647ecf7673214ec6ac7b1
Миша Мишин <spawn.rapture-0n@icloud.com>
2021-12-04 13:28:13 +0300

    Добавьте файлы проекта.

---
+++ Zadacha7.2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha7._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину ребра куба:");
+            double a = Convert.ToInt32(Console.ReadLine());
+            double V, S;
+            CubeVnS(a, out V, out S);
+            Console.WriteLine("Объем куба с заданной длиной ребра равен {0:f2}.",V);
+            Console.WriteLine("Площадь поверхности куба с заданной длиной ребра равна {0:f2}.", S);
+            Console.ReadKey();
+        }
+        static void CubeVnS(double a, out double V, out double S)
+        {
+            V = a*a*a;
+            S = a*a*6;
+        }
+    }
+}
